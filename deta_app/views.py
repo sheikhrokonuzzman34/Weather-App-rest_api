b0a48f8c427422d16e8503ea2f908451734cde6c
SheikhRokon <mdrokon.u545@gmail.com>
2023-11-26 15:50:15 +0600

    up date

--- deta_app/views.py
+++ deta_app/views.py
@@ -1,6 +1,24 @@
+import requests
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'data_app/index.html')
+    appid = '514f80e7635420a771d1e63c485c707f'
+    URL = 'https://api.openweathermap.org/data/2.5/weather'
+    PARAMS = {'q':'dhaka', 'appid':appid, 'units':'metric'}
+    
+    req = requests.get(url=URL, params=PARAMS)
+    respos = req.json()
+    description = respos['weather'][0]['description']
+    icon = respos['weather'][0]['icon']
+    temp = respos['main']['temp']
+    date = datetime.date.today()
+    context = {
+        'description': description,
+        'icon': icon,
+        'temp': temp,
+        'date': date,
+    }
+    return render(request, 'data_app/index.html',context)
